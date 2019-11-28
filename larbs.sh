f3774f5d52a52a1b88fee6438c6695f3dfb8d992
fleureclatant <55487996+fleureclatant@users.noreply.github.com>
2019-11-28 22:05:42 +0000

    changed script to my repo

--- larbs.sh
+++ larbs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+fleureclatant#!/bin/sh
 # Luke's Auto Rice Boostrapping Script (LARBS)
 # by Luke Smith <luke@lukesmith.xyz>
 # License: GNU GPLv3
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/fleureclatant/voidrice.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/fleureclatant/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
